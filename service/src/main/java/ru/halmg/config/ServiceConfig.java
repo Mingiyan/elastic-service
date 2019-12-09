2d011c3c7a9d793e6550f0b3b6a04f04c453d01a
Дорджиев Мингиян Вячеславович <Dordzhiev.M.V@sberbank.ru>
2019-12-09 16:52:06 +0300

    add service configuration

---
+++ service/src/main/java/ru/halmg/config/ServiceConfig.java
@@ -0,0 +1,38 @@
+package ru.halmg.config;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+@ComponentScan(basePackages = "ru.halmg.service")
+@PropertySource("classpath:application.properties")
+public class ServiceConfig {
+
+    @Value("${elasticsearch.host}")
+    private String elasticHost;
+    @Value("${elasticsearch.port}")
+    private int elasticPort;
+    @Value("${cluster.name}")
+    private String clusterName;
+
+
+    @Bean
+    public Client elasticClient() throws UnknownHostException {
+        Settings settings = Settings.EMPTY;
+        if (!"".equals(clusterName)) {
+            settings = Settings.builder().put("cluster.name", clusterName).build();
+        }
+        return new PreBuiltTransportClient(settings)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(elasticHost), elasticPort));
+    }
+}
